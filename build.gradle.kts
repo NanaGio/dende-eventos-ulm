0fbbab146b7d2ec34bcdd3cbf4b3f22545e7340c
João Mira <jmirauniversitario@gmail.com>
2026-02-07 18:42:57 -0300

    Inicializando projeto

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+}
+
+group = "org.oat1"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(23)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
